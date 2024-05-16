55ed74de72ead73c28f85937e0f7b2f8f6562d82
Linda <63205336+joulespergramcelsius@users.noreply.github.com>
2024-05-16 15:13:59 -0400

    Update basics.sh

--- basics.sh
+++ basics.sh
@@ -3,3 +3,6 @@ grep hypothetical dp-proteome.faa >hypo.faa
 
 # filter out conserved protein entries
 grep '>' dp-proteome.faa| grep -v 'hypothetical'>conserved.faa
+
+# sequence alignment
+fasta36 -m 8CC seq/mgstm1.nt seq/gst.nlib
