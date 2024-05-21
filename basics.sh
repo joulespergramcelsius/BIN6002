79989db3c7db9d3e42059ed9765bc09c0ad93a70
Linda <63205336+joulespergramcelsius@users.noreply.github.com>
2024-05-21 14:21:38 -0400

    Update basics.sh

--- basics.sh
+++ basics.sh
@@ -8,3 +8,4 @@ grep '>' dp-proteome.faa| grep -v 'hypothetical'>conserved-entries.txt
 fasta36 -m 8CC test-target.faa test-reference.faa > test-output.
 
 head -n 1721 conserved.faa > conserved-1721.faa
+grep -c ">" conserved-1721.faa
